c55c97d9b5222bad7dfb4e26221603324a3ff22e
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-24 07:47:24 +0000

    [finish]

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,13 +4,16 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   #has_many :favorited_users, througth: :favorites, source: :user
   #BookはFavoriteを通じてUserへの間接的に繋がる
+
+  has_many :week_favorites, -> { where(create_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search ==  "perfect"
       @book = Book.where("title LIKE?", "#{word}")
