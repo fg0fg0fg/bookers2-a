e986f3d458dc69515345ec50c5f04ea21c6a1ad8
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-24 02:05:45 +0000

    [finish]678a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book.increment!(:view_count)
     @user = @book.user
     @book_comment = BookComment.new
   end
